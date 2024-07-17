8f37619863e27bb8548656719ece865f87561b6c
allisonRabia <09snippet-frazzle@icloud.com>
2024-07-16 20:09:42 -0500

    tests 1-8 passed

--- rover.js
+++ rover.js
@@ -1,13 +1,24 @@
 class Rover {
   constructor(position, mode, generatorWatts) {
     this.position = position;
-    // if (!name) {
-    //   throw Error("Name required.");
-    // }
     this.mode = "NORMAL";
     this.generatorWatts = 110;
   }
+  receiveMessage(message) {
+    let response = { message: message.name, results: [] };
+    //let results = []
+    return response;
+  }
 }
 
-//recieveMessage(message);
+// let commands = [
+//   new Command("MODE_CHANGE", "LOW_POWER"),
+//   new Command("STATUS_CHECK"),
+// ];
+// let message = new Message("Test message with two commands", commands);
+// let rover = new Rover(98382); // Passes 98382 as the rover's position.
+// let response = rover.receiveMessage(message);
+
+//console.log(response.name);
+
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -4,6 +4,7 @@ const Command = require("../command.js");
 
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
+
 //Test 7 (like test 2 or 3 or 5 or 6)
 describe("Rover class", function () {
   it("constructor sets position and default values for mode and generatorWatts”", function () {
@@ -11,5 +12,19 @@ describe("Rover class", function () {
     expect(rover.position).toEqual(98382);
     expect(rover.mode).toEqual("NORMAL");
     expect(rover.generatorWatts).toEqual(110);
+  });
+});
+
+//Test 8
+describe("Rover Class", function () {
+  it("response returned by receiveMessage contains the name of the message", function () {
+    let commands = [
+      new Command("MODE_CHANGE", "LOW_POWER"),
+      new Command("STATUS_CHECK"),
+    ];
+    let message = new Message("Test message with two commands", commands);
+    let rover = new Rover(98382); // Passes 98382 as the rover's position.
+    let response = rover.receiveMessage(message);
+    expect(response.message).toEqual("Test message with two commands");
   });
 });
